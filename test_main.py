964ef1383b3efcbbcf2cc23425c83345f5561248
Shaquille004 <shaquillesma04@gmail.com>
2024-10-02 17:05:54 -0400

    Agregar Hola Mundo y prueba unitaria

---
+++ test_main.py
@@ -0,0 +1,8 @@
+import unittest
+from main import hola_mundo
+
+class TestHolaMundo (unittest.TestCase):
+    def test_hola_mundo(self):
+        self.assertEqual(hola_mundo(), "Hola, Mundo!")
+        if __name__=='__main__':
+            unittest.main()
